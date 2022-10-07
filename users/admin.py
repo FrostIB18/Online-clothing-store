69a3fe53981050f1b054b58631f937887e4f3c03
FrostIB <111425124+FrostIB18@users.noreply.github.com>
2022-10-07 18:59:17 +0600

    Delete users directory

--- users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from users.models import User
-from products.admin import BasketAdminInline
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    inlines = (BasketAdminInline,)
